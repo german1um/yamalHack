6828d6be48f5c0515d373c871741837c784cca05
gera <german_abramov@epam.com>
2019-10-21 23:35:50 +0300

    use custom models for face api

--- src/main/kotlin/io/terra/yamalHack/api/entity/DetectFaceApiResponse.kt
+++ src/main/kotlin/io/terra/yamalHack/api/entity/DetectFaceApiResponse.kt
@@ -1,7 +1,5 @@
 package io.terra.yamalHack.api.entity
 
-import com.microsoft.azure.cognitiveservices.faceapi.FaceAttributes
-
 data class DetectFaceApiResponse (
         val faceId: String,
         val faceAttributes: FaceAttributes
