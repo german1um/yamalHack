3fd08bf388cbcd0b9410f46179bf8b8c73ac76eb
gera <german_abramov@epam.com>
2019-10-22 22:55:04 +0300

    max pic size bigger, because size matters

--- src/main/kotlin/io/terra/yamalHack/config/CommonConfig.java
+++ src/main/kotlin/io/terra/yamalHack/config/CommonConfig.java
@@ -2,6 +2,7 @@ package io.terra.yamalHack.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @Configuration
@@ -9,7 +10,7 @@ public class CommonConfig {
     @Bean(name = "multipartResolver")
     public CommonsMultipartResolver multipartResolver() {
         CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-        multipartResolver.setMaxUploadSize(100000);
+        multipartResolver.setMaxUploadSize(10000000);
         return multipartResolver;
     }
 }
